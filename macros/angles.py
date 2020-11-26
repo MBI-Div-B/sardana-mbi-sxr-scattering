d8238f01b36fcbee521c344752c187ad45ff7088
Daniel Schick <schick@mbi-berlin.de>
2020-11-26 16:18:45 +0100

    add an and uan macros

---
+++ macros/angles.py
@@ -0,0 +1,18 @@
+from sardana.macroserver.macro import Macro, macro, Type
+
+@macro([
+        ['tth_pos', Type.Float, None, 'two_theta position'],
+        ['th_pos', Type.Float, None, 'theta position'],
+    ])
+def an(self, tth_pos, th_pos):
+    """moving tth and th at the same time"""
+    self.execMacro(['mv', [['tth', tth_pos], ['th', th_pos]]])
+
+
+@macro([
+        ['tth_pos', Type.Float, None, 'two_theta position'],
+        ['th_pos', Type.Float, None, 'theta position'],
+    ])
+def uan(self, tth_pos, th_pos):
+    """update moving tth and th at the same time"""
+    self.execMacro(['umv', [['tth', tth_pos], ['th', th_pos]]])
