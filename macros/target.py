053039b8fbceb8d53af705516934e8c6dcfb28d6
Daniel Schick <schick@mbi-berlin.de>
2020-11-26 16:20:17 +0100

    rename tape_start stop to on off

--- macros/target.py
+++ macros/target.py
@@ -1,24 +1,24 @@
-from sardana.macroserver.macro import Macro, macro, Type
-from tango import DeviceProxy
+from sardana.macroserver.macro import Macro, macro, imacro, Type
+from tango import DeviceProxy, DevState
 from time import sleep
 
 @macro()
-def tape_start(self):
-    """Macro tape_start"""
-    self.output("Running tape_start...")
+def tape_on(self):
+    """Macro tape_on"""
+    self.output("Running tape_on...")
     proxy = DeviceProxy('xpl/debristape/rsxs')
     proxy.start_all()
     sleep(0.5)
-    self.output("Tape started!")
+    self.output("Tape is ON!")
 
 
 @macro()
-def tape_stop(self):
-    """Macro tape_stop"""
-    self.output("Running tape_stop...")
+def tape_off(self):
+    """Macro tape_off"""
+    self.output("Running tape_off...")
     proxy = DeviceProxy('xpl/debristape/rsxs')
     proxy.stop_all()
-    self.output("Tape stopped!")
+    self.output("Tape is OFF!")
 
 
 @macro()
@@ -32,4 +32,26 @@ def tape_test(self):
     proxy.stop_all()
     self.output("Tape stopped!")
 
+
+
+@imacro()
+def tape_check(self):
+    """Macro tape_check"""
+    proxy = DeviceProxy('xpl/debristape/rsxs')
+    if proxy.State() == DevState.MOVING:
+        # all fine
+        pass
+    else:
+        # debris tape is not moving
+        self.output('Debris tape is NOT moving!')
+        answer = ''
+        while answer not in ['y', 'n']:
+            answer = self.input("Start debris tape (y)?")
+
+        if answer == 'n':
+            self.output('ct without running tape!')
+        else:
+            proxy.start_all()
+            sleep(0.5)
+            self.output("Tape is ON!")
 
